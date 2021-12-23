1d5f4f117e79911b34825084180c8d75328f8773
Gurpre3tW <gurpreet0warval@gmail.com>
2021-12-23 16:32:48 +0530

    initial generator is done.

---
+++ generator.py
@@ -0,0 +1,28 @@
+# Password Generator
+import string
+from random import choice, randint
+
+
+class Generator : 
+
+    def __init__(self, num):
+
+        # List of all letters numbers and symbols in form of string .
+        self.letters = [
+            string.ascii_uppercase, 
+            string.ascii_lowercase, 
+            string.digits, 
+            string.punctuation
+        ]
+        self.num = int(num)
+
+    #Generating the password    
+    def generate(self):
+        self.word = ""
+        for i in range (self.num):
+            self.word = self.word + choice(self.letters[randint(0,3)])
+        
+        return self.word
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # Password manager
+
+print("Wellcome to Password manager")
+
+def manual():
+
+    print("""
+        Enter 'q' to quit 
+        Enter 'h' for help
+        Enter 'n' for new entry    
+    """)
+
+manual()
+while True:
+
+    inp = input("-> ")
+
+    if inp.lower() == 'q':
+        break
+    elif inp.lower() == 'h':
+        manual()
+    elif inp.lower == 'n':
+        pass
+    else:
+        print("Please enter correct input or Enter 'h' for help")
+
+
